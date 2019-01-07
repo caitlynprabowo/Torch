54203532ea7b383f8f79eb7988a29cb9b7b01d74
Caitlyn Audrey Prabowo <Caitlyn@macbook-pro.local>
2019-01-07 21:53:11 +0700

    Initial Commit

---
+++ Torch/ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Torch
+//
+//  Created by Caitlyn Audrey Prabowo on 30/10/18.
+//  Copyright © 2018 Caitlyn Audrey Prabowo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var lightOn = true
+    
+
+    @IBAction func buttonPress(_ sender: Any) {
+        
+        lightOn = !lightOn
+        
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        if lightOn {
+            view.backgroundColor = .white
+            
+        } else {
+            view.backgroundColor = .black
+           
+        }
+
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
+
